843c32077c9f5f61817b72f53ad83aa7a092785d
Muhammad Younas <muhammad.younas@atomicity-interactive.com>
2022-11-21 15:19:09 +0500

    Implemented CRUD using EFC Code First Approach

--- Services/impl/UsersService.cs
+++ Services/impl/UsersService.cs
@@ -13,12 +13,7 @@ namespace UserEntity.Services.impl
     // Service class for the User Entity which is implemented from the Interface
     public class UsersService : IUsersService
     {
-        // mock list for the WEBAPI CRUD Implementation
-        public static List<User> user = new List<User>()
-        {   new User {Id=1,FirstName="qwer",LastName="eq",UserName="qwer12",Email="qwer12@gmail.com",Password="qwer1212"},
-         new User {Id=2,FirstName="qwer",LastName="eq",UserName="qwer12",Email="qwer12@gmail.com",Password="qwer1212"}
 
-        };
         private readonly IMapper _mapper;
         private readonly UserDbContext _db;
         public UsersService(IMapper mapper, UserDbContext db)
@@ -30,7 +25,7 @@ namespace UserEntity.Services.impl
         public async Task<List<UserDto>> AddUsers(UserDto addUser)
         {
             // checking if email is exist, if exist it will becomes true then we will not step into the if condition
-            var isEmailExist = user.Any(x => x.Email == addUser.Email);
+            var isEmailExist = _db.Users.Any(x => x.Email == addUser.Email);
 
             // if email not exist, we will further move into the login functionality
          // ||...
@@ -39,11 +34,11 @@ namespace UserEntity.Services.impl
             {
                 // user is added into our Users List
                 var data = _mapper.Map<User>(addUser);
-                data.Id = user.Max(x => x.Id) + 1;
-                user.Add(data);
+                await _db.Users.AddAsync(data);
+                await _db.SaveChangesAsync();
                 // return the user!
          
-                return (user.Select(x => _mapper.Map<UserDto>(x))).ToList();
+                return (_db.Users.Select(x => _mapper.Map<UserDto>(x))).ToList();
             }
             else
             {
@@ -56,9 +51,11 @@ namespace UserEntity.Services.impl
         {
             try
             {
-                User isUserExist = user.First(x => x.Id == id);
-                user.Remove(isUserExist);
-                var data = (user.Select(x => _mapper.Map<UserDto>(x))).ToList();
+                var dbDelUser = await _db.Users.FirstAsync(x => x.Id == id);
+                _db.Users.Remove(dbDelUser);
+                await _db.SaveChangesAsync();
+
+                var data = (_db.Users.Select(x => _mapper.Map<UserDto>(x))).ToList();
                 return data;
             }
             catch (Exception ex)
@@ -80,7 +77,8 @@ namespace UserEntity.Services.impl
         public async Task<UserDto> GetById(int id)
         {
             // will return the first found obj otherwise null 
-            var result = _mapper.Map<UserDto>(user.FirstOrDefault(x => x.Id == id));
+            var dbUser = await _db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            var result = _mapper.Map<UserDto>(dbUser);
             // if not null the details of a particular user will return
             if (result != null)
             {
@@ -94,23 +92,22 @@ namespace UserEntity.Services.impl
             }
         }
         // update the user by sending the PUT from the PSOTMAN
-        public async Task<UserDto> Update(UserDto newUser)
+        public async Task<UserDto> Update(UserDto updateUser)
         {
-            // getting the user data for updating if id found and email should be null
-            var userObj = user.FirstOrDefault(u => u.Id == newUser.Id && newUser.Email is null);
-            // if userObj is null due to no userName found , will throw an exception inthe IF condition
-            if (userObj == null)
+
+            try
             {
-                // this will throw an exception for our Catch block to generate a generic response error msg
-                throw new Exception();
+                var dbUpdate = await _db.Users.FirstOrDefaultAsync(x => x.Id == updateUser.Id && string.IsNullOrEmpty(updateUser.Email));
+                dbUpdate.UserName = updateUser.UserName;
+                dbUpdate.Password = updateUser.Password;
+                _db.Users.Update(dbUpdate);
+                await _db.SaveChangesAsync();
+                return _mapper.Map<UserDto>(dbUpdate);
             }
-            else
+            catch (Exception ex)
             {
-                // if userFound for update it will update the user
-                userObj.UserName = newUser.UserName;
-                userObj.Password = newUser.Password;
-                // will return UserObj into the Try Block of the UpdateUser IAction
-                return _mapper.Map<UserDto>(userObj);
+
+                throw ex;
             }
         }
     }
